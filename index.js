4138de35df4ad07211894c59fb6a467a3f0f68d2
Kenan Dolic <kdolic@alumni.iastate.edu>
2021-03-11 11:43:42 -0600

    wip

---
+++ index.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+const express = require('express')
+
+
+const server = express()
+
+server.use(express.json())
+
+if(process.env.NODE_ENV === 'development'){ // on Heroku machine, an env variable is called "NODE_ENV" -> "production"
+    const cors = require('cors')
+    server.use(cors())
+}
