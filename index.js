b2e0c27aec9464004a94eaa9eb6d2b40c4f48729
Kenan Dolic <kdolic@alumni.iastate.edu>
2021-03-11 12:30:55 -0600

    wip

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 require('dotenv').config()
+const path = require('path')
 const express = require('express')
 
 
 const server = express()
 
 server.use(express.json())
+server.use(express.static(path.join(__dirname, 'client/build')))
 
 if(process.env.NODE_ENV === 'development'){ // on Heroku machine, an env variable is called "NODE_ENV" -> "production"
     const cors = require('cors')
     server.use(cors())
 }
 
-server.use('*', (req, res) => {
-    res.send('<h1>Kenan API</h1>')
+// our API comes earlier in the pipeline
+server.get('/api/hello', (req, res) => {
+    res.json({ message: 'hello'})
 })
 
+// catch-all that just sends back index.html
+server.get('*', (req, res) => {
+   res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
+})
 
 const PORT = process.env.PORT || 4000
 
