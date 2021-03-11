a7a9077ebf7aa92c2e54a2f1dcf99430fdee8f1f
Kenan Dolic <kdolic@alumni.iastate.edu>
2021-03-11 13:01:50 -0600

    wip

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,28 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
+function getUrl(path) {
+  if(process.env.NODE_ENV === 'development'){
+    return 'http://localhost:5000' + path
+  } else {
+    return path
+  }
+}
+
 function App() {
+  const [message, setMessage] = useState('Hello Kenan')
+  useEffect(() => {
+    fetch(getUrl('/api/hello'))
+    .then(res => res.json())
+    .then(resBody => setMessage(resBody.message))
+    .catch(err => {
+      debugger
+    })
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       {message}
       </header>
     </div>
   );
